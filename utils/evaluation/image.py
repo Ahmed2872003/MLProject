475e5238e1162e29918481dcc3de301e6ad9d4aa
= <=>
2024-12-05 04:40:49 +0200

    .

--- utils/evaluation/image.py
+++ utils/evaluation/image.py
@@ -1,6 +1,8 @@
 from sklearn.metrics import accuracy_score, confusion_matrix,precision_score, recall_score, roc_curve, roc_auc_score
 from sklearn.preprocessing import label_binarize
+from sklearn.linear_model import LogisticRegressionCV
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def evaluate(model, Y_test, Y_pred, X_test):
@@ -21,12 +23,32 @@ def evaluate(model, Y_test, Y_pred, X_test):
 
     eval_info["auc_curve_info"]["Y_test_bin"] = y_test_bin = label_binarize(Y_test, classes=np.unique(np.array(Y_test))) 
 
-   
-    fpr, tpr, _ = roc_curve(y_test_bin.ravel(), y_score.ravel())
-
     eval_info["AUC"] = round(roc_auc_score(y_test_bin, y_score, multi_class="ovr"), 2)
 
     return eval_info
 
 
-# np.unique(np.array(Y_image_test))
+
+
+def show_roc_graph(title, Y_test_bin, Y_score):
+    for i in range(Y_score.shape[1]): 
+        fpr, tpr, _ = roc_curve(Y_test_bin[:, i], Y_score[:, i])
+        plt.plot(fpr, tpr, label=f"Class {i}")
+
+    plt.plot([0, 1], [0, 1], 'k--', label="Chance")
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title(title)
+    plt.legend(loc="lower right")
+    plt.show()
+
+
+def show_logestic_Reg_loss_curve(X_train, Y_train):
+    logreg_cv = LogisticRegressionCV(max_iter=1000, random_state=42, solver='liblinear')
+    logreg_cv.fit(X_train, Y_train)
+
+    plt.plot(logreg_cv.scores_[1].mean(axis=0))
+    plt.xlabel('Iterations')
+    plt.ylabel('Log Loss')
+    plt.title('Logistic Regression Loss Curve')
+    plt.show()
